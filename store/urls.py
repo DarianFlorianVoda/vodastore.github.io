faf9f3439f5d6d308c89487ef85b5a2a8999e47c
DarianV <darian.voda00@e-uvt.ro>
2022-01-02 21:50:07 +0200

    Added Stripe checkout Added Excel export

--- store/urls.py
+++ store/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
+from django.urls import path, include
 from django.urls import path
 from .views.home import Index , store
 from .views.signup import Signup
 from .views.login import Login , logout
 from .views.cart import Cart
-from .views.checkout import CheckOut
+from .views.checkout import CheckOut, CreateCheckoutSessionView, paymentSuccess, paymentCancel, my_webhook_view, export
 from .views.orders import OrderView
 from .middlewares.auth import  auth_middleware
 
@@ -12,13 +13,15 @@ from .middlewares.auth import  auth_middleware
 urlpatterns = [
     path('', Index.as_view(), name='homepage'),
     path('store', store , name='store'),
-    
     path('signup', Signup.as_view(), name='signup'),
     path('login', Login.as_view(), name='login'),
     path('logout', logout , name='logout'),
     path('cart', auth_middleware(Cart.as_view()) , name='cart'),
     path('check-out', CheckOut.as_view() , name='checkout'),
-    path('orders', auth_middleware(OrderView.as_view()), name='orders')
-
-
+    path('orders', auth_middleware(OrderView.as_view()), name='orders'),
+    path('create-checkout-session', CreateCheckoutSessionView.as_view(), name='create-checkout-session'),
+    path('payment-success/', paymentSuccess, name='payment-success'),
+    path('payment-cancel/', paymentCancel, name='payment-cancel'),
+    path('webhook/stripe', my_webhook_view, name='webhook-stripe'),
+    path('export', export, name='export')
 ]
--- store/views/checkout.py
+++ store/views/checkout.py
@@ -1,6 +1,13 @@
+import csv
+import datetime
+
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
 
 from django.contrib.auth.hashers import check_password
+from django.urls import reverse
+from django.views.decorators.csrf import csrf_exempt
+
 from store.models.customer import Customer
 from django.views import View
 
@@ -29,3 +36,118 @@ class CheckOut(View):
         request.session['cart'] = {}
 
         return redirect('cart')
+
+
+#Stripe
+import stripe
+from django.conf import settings
+# This is your test secret API key.
+stripe.api_key = settings.STRIPE_SECRET_KEY
+endpoint_secret = settings.STRIPE_WEBHOOK_SECRET
+
+class CreateCheckoutSessionView(View):
+    def post(self, request,  *args, **kwargs):
+        address = request.POST.get('address')
+        phone = request.POST.get('phone')
+        customer = request.session.get('customer')
+        cart = request.session.get('cart')
+        products = Products.get_products_by_id(list(cart.keys()))
+        host = self.request.get_host()
+        #order_id = request.POST.get('order-id')
+        for product in products:
+            order = Order(customer=Customer(id=customer),
+                          product=product,
+                          price=product.price,
+                          address=address,
+                          phone=phone,
+                          quantity=cart.get(str(product.id)))
+            #order = Order.objects.get(id=order_id)
+            order.save()
+        checkout_session = stripe.checkout.Session.create(
+
+            line_items=[
+                {
+                    'price_data': {
+                        'currency': 'eur',
+                        'unit_amount': order.price * 100,
+                        'product_data':{
+                            'name': order.id,
+                        },
+                    },
+                    'quantity': 1,
+                },
+            ],
+
+            mode='payment',
+            success_url=f"http://{host}{reverse('payment-success')}",
+            cancel_url=f"http://{host}{reverse('payment-cancel')}",
+        )
+        request.session['cart'] = {}
+        return redirect(checkout_session.url, code=303)
+
+
+def paymentSuccess(request):
+    context = {
+        'payment_status': 'success'
+    }
+    return render(request, 'confirmation.html', context)
+
+def paymentCancel(request):
+    context = {
+        'payment_status': 'cancel'
+    }
+    return render(request, 'confirmation.html', context)
+
+@csrf_exempt
+def my_webhook_view(request):
+    payload = request.body
+    sig_header = request.META['HTTP_STRIPE_SIGNATURE']
+    event = None
+
+    try:
+        event = stripe.Webhook.construct_event(
+            payload, sig_header, endpoint_secret
+        )
+    except ValueError as e:
+        # Invalid payload
+        return HttpResponse(status=400)
+    except stripe.error.SignatureVerificationError as e:
+        # Invalid signature
+        return HttpResponse(status=400)
+
+    if event['type'] == 'checkout.session.completed':
+        session = event['data']['object']
+
+        if session.payment_status == "paid":
+            #Fullfill the purchase
+            line_item = session.list_line_items(session.id, limit=1).data[0]
+            order_id = line_item['description']
+            fulfill_order(order_id)
+
+    # Passed signature verification
+    return HttpResponse(status=200)
+
+def fulfill_order(order_id):
+    order = Order.objects.get(id=order_id)
+    order.ordered = True
+    order.orderDate = datetime.datetime.now()
+    order.save()
+
+    for item in order.items.all():
+        product_var = Order.objects.get(id=item.product.id)
+        product_var.stock -= item.quantity
+        product_var.save()
+
+
+def export(request):
+    response = HttpResponse(content_type='text/csv')
+
+    writer = csv.writer(response)
+    writer.writerow(['Name', 'Price', 'Category', 'Description'])
+
+    for product in Products.objects.all().values_list('name', 'price', 'category', 'description'):
+        writer.writerow(product)
+
+    response['Content-Disposition'] = 'attachment; filname="vodaproducts.csv"'
+
+    return response
